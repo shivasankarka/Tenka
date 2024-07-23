36ea231d36905dd6b9cc7d46a98d4d4f0819774f
darth-photon <shivasankar.ka@gmail.com>
2024-07-24 00:40:47 +0900

    added get_all_envs anf fixed cli

--- src/environments.py
+++ src/environments.py
@@ -4,10 +4,13 @@ import sys
 import datetime
 import shutil
 
-def get_envs():
-    pass
+def get_all_envs():
+    with open(os.path.expanduser('~/.tenka/environments.json'), 'r') as f:
+        envs = json.load(f)
+        for env in envs:
+            print(f"{env}- Mojo@{envs[env]['version']}")
 
-def create_environment(env_name, version: str = "24.4.0"):
+def create_environment(env_name: str, version: str = "24.4.0"):
     with open(os.path.expanduser('~/.tenka/environments.json'), 'r') as f:
         envs = json.load(f)
         if env_name in envs:
@@ -26,7 +29,7 @@ def create_environment(env_name, version: str = "24.4.0"):
                 json.dump(envs, f, indent=4)  
             sys.exit(1)
 
-def delete_environment(env_name):
+def delete_environment(env_name: str):
     with open(os.path.expanduser('~/.tenka/environments.json'), 'r') as f:
         envs = json.load(f)
         if env_name in envs:
@@ -38,13 +41,13 @@ def delete_environment(env_name):
         else:
             sys.exit(0)
 
-def change_cfg(env_name_init, env_name_new):
+def edit_cfg(env_name_init: str, env_name_final: str):
     home_dir = os.path.expanduser("~/.tenka")
     with open(os.path.join(home_dir, "modular.cfg"), "r+") as file:
         content = file.read()
         if env_name_init == "mojo":
-            content = content.replace(os.path.join(os.path.expanduser("~"), ".modular/pkg/packages.modular.com_mojo"), f"{home_dir}/envs/{env_name_new}")
-        if env_name_new == "mojo":
+            content = content.replace(os.path.join(os.path.expanduser("~"), ".modular/pkg/packages.modular.com_mojo"), f"{home_dir}/envs/{env_name_final}")
+        if env_name_final == "mojo":
             content = content.replace(f"{home_dir}/envs/{env_name_init}", os.path.join(os.path.expanduser("~"), ".modular/pkg/packages.modular.com_mojo"))
         else:
             content = content.replace(f"{home_dir}/envs/{env_name_init}", os.path.join(home_dir, "envs", env_name_new))
@@ -52,9 +55,9 @@ def change_cfg(env_name_init, env_name_new):
         file.write(content)
         file.truncate()
    
-def change_active_env(env_name_new):
+def change_active_env(env_name_new: str, version: str):
     with open(os.path.expanduser('~/.tenka/active.json'), 'w+') as f:
-        json.dump({'active': env_name_new}, f)
+        json.dump({'active': env_name_new, 'version': version}, f)
     
 def get_active_environment():
     with open(os.path.expanduser('~/.tenka/active.json'), 'r') as f:
@@ -62,25 +65,30 @@ def get_active_environment():
     print(envs['active'])
 
 if __name__ == "__main__":
-    # if (len(sys.argv) != 3 and (sys.argv[1] != "change_cfg" or sys.argv[1] != "change_active_env")) and (len(sys.argv) != 2 or sys.argv[1] not in ["create_environment", "delete_environment", "get_active_environment"]):
-    #     print("Usage: python environments.py create_environment <env_name>")
-    #     print("Usage: python environments.py delete_environment <env_name>")
-    #     print("Usage: python environments.py get_active_environment")
-    #     print("Usage: python environments.py change_cfg <env_name_init> <env_name_new>")
-    #     print("Usage: python environments.py change_active_env <env_name_init> <env_name_new>")
-    #     sys.exit(1)
-
-    if sys.argv[1] == "create_environment" and len(sys.argv) == 3:
-        create_environment(sys.argv[2])
-    elif sys.argv[1] == "delete_environment" and len(sys.argv) == 3:
-        delete_environment(sys.argv[2])
-    elif sys.argv[1] == "change_cfg" and len(sys.argv) == 4:
-        change_cfg(sys.argv[2], sys.argv[3])
-    elif sys.argv[1] == "change_active_env" and len(sys.argv) == 3:
-        change_active_env(sys.argv[2])
-    elif sys.argv[1] == "get_active_environment" and len(sys.argv) == 2:
-        get_active_environment()
-    else:
-        print("Invalid command or arguments")
+    if len(sys.argv) < 2:
+        print("Usage: python environments.py <command> [<args>]")
         sys.exit(1)
 
+    command = sys.argv[1]
+
+    if command == "create_environment" and len(sys.argv) == 3:
+        create_environment(sys.argv[2])
+    elif command == "delete_environment" and len(sys.argv) == 3:
+        delete_environment(sys.argv[2])
+    elif command == "edit_cfg" and len(sys.argv) == 4:
+        edit_cfg(sys.argv[2], sys.argv[3])
+    elif command == "change_active_env" and len(sys.argv) == 4:
+        change_active_env(sys.argv[2], sys.argv[3])
+    elif command == "get_active_environment" and len(sys.argv) == 2:
+        get_active_environment()
+    elif command == "get_all_envs" and len(sys.argv) == 2:
+        get_all_envs()
+    else:
+        print("Invalid command or arguments")
+        print("Available commands:")
+        print("  create_environment <env_name>")
+        print("  delete_environment <env_name>")
+        print("  edit_cfg <env_name_init> <env_name_final>")
+        print("  change_active_env <env_name> <version>")
+        print("  get_active_environment")
+        sys.exit(1)

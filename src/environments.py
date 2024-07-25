a6d58aeeb365d4ec7f787a07d426122fba6ad633
darth-photon <shivasankar.ka@gmail.com>
2024-07-25 10:15:20 +0900

    added mojo version numbers map

--- src/environments.py
+++ src/environments.py
@@ -21,12 +21,19 @@ def create_environment(env_name: str, version: str = "24.4.0"):
                     {'name': env_name, 
                      'version': version,
                      'description': f"Environment {env_name} with Mojo {version}",
-                     'created': datetime.datetime.now().isoformat()
+                     'created': datetime.datetime.now().isoformat(),
+                     'packages': []
                      }
             }
             envs.update(new_env)
             with open(os.path.expanduser('~/.tenka/environments.json'), 'w+') as f:
                 json.dump(envs, f, indent=4)  
+            with open(os.path.expanduser('~/.tenka/envs/'+env_name+'/modular.cfg'), 'r+') as cfg:
+                content = cfg.read()
+                content = content.replace(os.path.expanduser('~/.tenka/envs/base'), os.path.expanduser('~/.tenka/envs/'+env_name))
+                cfg.seek(0)
+                cfg.write(content)
+                cfg.truncate()
             sys.exit(1)
 
 def delete_environment(env_name: str):
@@ -36,26 +43,48 @@ def delete_environment(env_name: str):
             envs.pop(env_name)
             with open(os.path.expanduser('~/.tenka/environments.json'), 'w+') as f:
                 json.dump(envs, f, indent=4)
-            shutil.rmtree(os.path.expanduser(f'~/.tenka/envs/{env_name}'))
             sys.exit(1)
         else:
             sys.exit(0)
 
 def edit_cfg(env_name_init: str, env_name_final: str):
     home_dir = os.path.expanduser("~/.tenka")
-    with open(os.path.join(home_dir, "modular.cfg"), "r+") as file:
+    with open(os.path.join(home_dir, "envs", env_name_init, "modular.cfg"), "r+") as file:
         content = file.read()
         if env_name_init == "mojo":
             content = content.replace(os.path.join(os.path.expanduser("~"), ".modular/pkg/packages.modular.com_mojo"), f"{home_dir}/envs/{env_name_final}")
         if env_name_final == "mojo":
             content = content.replace(f"{home_dir}/envs/{env_name_init}", os.path.join(os.path.expanduser("~"), ".modular/pkg/packages.modular.com_mojo"))
         else:
-            content = content.replace(f"{home_dir}/envs/{env_name_init}", os.path.join(home_dir, "envs", env_name_new))
+            content = content.replace(f"{home_dir}/envs/{env_name_init}", os.path.join(home_dir, "envs", env_name_final))
         file.seek(0)
         file.write(content)
         file.truncate()
    
-def change_active_env(env_name_new: str, version: str):
+def change_active_env(env_name_new: str):
+    version_map = {
+    "24.4.0": 14,
+    "24.3.0": 13,
+    "24.2.1": 12,
+    "24.2.0": 11,
+    "24.1.1": 10,
+    "24.1.0": 9,
+    "0.7.0": 8,
+    "0.6.1": 7,
+    "0.6.0": 6,
+    "0.5.0": 5,
+    "0.4.0": 4,
+    "0.3.1": 3,
+    "0.3.0": 2,
+    "0.2.1": 1
+    }
+    if env_name_new == "mojo":
+        version = list(version_map.keys())[0]
+    else:
+        with open(os.path.expanduser('~/.tenka/environments.json'), 'r') as f:
+            envs = json.load(f)
+            if env_name_new in envs:
+                version = envs[env_name_new]['version']
     with open(os.path.expanduser('~/.tenka/active.json'), 'w+') as f:
         json.dump({'active': env_name_new, 'version': version}, f)
     
@@ -71,14 +100,14 @@ if __name__ == "__main__":
 
     command = sys.argv[1]
 
-    if command == "create_environment" and len(sys.argv) == 3:
-        create_environment(sys.argv[2])
+    if command == "create_environment" and len(sys.argv) == 4:
+        create_environment(sys.argv[2], sys.argv[3])
     elif command == "delete_environment" and len(sys.argv) == 3:
         delete_environment(sys.argv[2])
     elif command == "edit_cfg" and len(sys.argv) == 4:
         edit_cfg(sys.argv[2], sys.argv[3])
-    elif command == "change_active_env" and len(sys.argv) == 4:
-        change_active_env(sys.argv[2], sys.argv[3])
+    elif command == "change_active_env" and len(sys.argv) == 3:
+        change_active_env(sys.argv[2])
     elif command == "get_active_environment" and len(sys.argv) == 2:
         get_active_environment()
     elif command == "get_all_envs" and len(sys.argv) == 2:
@@ -91,4 +120,5 @@ if __name__ == "__main__":
         print("  edit_cfg <env_name_init> <env_name_final>")
         print("  change_active_env <env_name> <version>")
         print("  get_active_environment")
+        print("  get_all_envs")
         sys.exit(1)
